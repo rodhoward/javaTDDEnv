2dea2e61c6542941f8323f62b5dedde0cbd4f2d9
rodhoward <rod.howard@gmail.com>
2017-04-18 22:54:36 -0700

    adding mockito lib for test double suport and using it in a silly example.

--- test/src/setup/test/HookupTest.java
+++ test/src/setup/test/HookupTest.java
@@ -1,12 +1,15 @@
 package setup.test;
 
 import org.junit.Test;
+import org.junit.Assert;
 import setup.Hookup;
+import static org.mockito.Mockito.mock;
 
 public class HookupTest {
 
     @Test
     public void isThisWorking() throws Exception {
-        new Hookup();
+        Hookup h =  mock(Hookup.class);
+        Assert.assertTrue(h instanceof Hookup);
     }
 }
